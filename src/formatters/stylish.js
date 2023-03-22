c139ea209beedc43b854b0d8d3876f91777cf1ce
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2023-03-22 20:36:58 +0100

    upd after 1 check

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,46 +7,46 @@ export default (value) => {
       return `${data}`;
     }
     const keys = Object.keys(data).sort();
-    let result = '{\n';
+    const result = ['{'];
     const getPrefix = replacer.repeat(spacesCount * depth);
     for (const key of keys) {
       const item = data[key];
       if (!isObject(item)) {
-        result += `${getPrefix}${key}: ${item}\n`;
+        result.push(`${getPrefix}${key}: ${item}`);
       } else {
-        result += `${getPrefix}${key}: ${stringify(item, depth + 1)}\n`;
+        result.push(`${getPrefix}${key}: ${stringify(item, depth + 1)}`);
       }
     }
-    result += `${replacer.repeat(spacesCount * (depth - 1))}}`;
-    return result;
+    result.push(`${replacer.repeat(spacesCount * (depth - 1))}}`);
+    return result.join('\n');
   };
 
   const printInner = (data, depth) => {
     const getPrefix = replacer.repeat(spacesCount * depth - 2);
     const keys = Object.keys(data).sort();
-    let result = '{\n';
+    const result = ['{'];
     for (const key of keys) {
       const item = data[key];
       if (item.type === 'added') {
-        result += `${getPrefix}+ ${key}: ${stringify(item.value, depth + 1)}\n`;
+        result.push(`${getPrefix}+ ${key}: ${stringify(item.value, depth + 1)}`);
       }
       if (item.type === 'deleted') {
-        result += `${getPrefix}- ${key}: ${stringify(item.oldValue, depth + 1)}\n`;
+        result.push(`${getPrefix}- ${key}: ${stringify(item.oldValue, depth + 1)}`);
       }
       if (item.type === 'unchanged') {
         if (item.valueType === 'simple') {
-          result += `${getPrefix}  ${key}: ${stringify(item.value, depth + 1)}\n`;
+          result.push(`${getPrefix}  ${key}: ${stringify(item.value, depth + 1)}`);
         } else {
-          result += `${getPrefix}  ${key}: ${printInner(item.value, depth + 1)}\n`;
+          result.push(`${getPrefix}  ${key}: ${printInner(item.value, depth + 1)}`);
         }
       }
       if (item.type === 'changed') {
-        result += `${getPrefix}- ${key}: ${stringify(item.oldValue, depth + 1)}\n`;
-        result += `${getPrefix}+ ${key}: ${stringify(item.newValue, depth + 1)}\n`;
+        result.push(`${getPrefix}- ${key}: ${stringify(item.oldValue, depth + 1)}`);
+        result.push(`${getPrefix}+ ${key}: ${stringify(item.newValue, depth + 1)}`);
       }
     }
-    result += `${replacer.repeat(spacesCount * (depth - 1))}}`;
-    return result;
+    result.push(`${replacer.repeat(spacesCount * (depth - 1))}}`);
+    return result.join('\n');
   };
   return printInner(value, 1);
 };
