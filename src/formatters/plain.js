51cb1b49a4d3d1201b842d8738bbc046cefa404a
Анастасия Левчук <nastya@MacBook-Air-Anastasia.local>
2023-03-24 09:02:14 +0100

    upd plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,22 +14,24 @@ export default (value) => {
   const printInner = (data, prefix) => {
     const printKey = (k) => [...prefix, k].join('.');
     const keys = Object.keys(data).sort();
-    const result = [];
-    for (const key of keys) {
-      const item = data[key];
-      if (item.type === 'unchanged' && item.valueType === 'complex') {
-        result.push(printInner(item.value, [...prefix, key]));
-      }
-      if (item.type === 'added') {
-        result.push(`Property '${printKey(key)}' was added with value: ${printValue(item.value)}`);
-      }
-      if (item.type === 'deleted') {
-        result.push(`Property '${printKey(key)}' was removed`);
-      }
-      if (item.type === 'changed') {
-        result.push(`Property '${printKey(key)}' was updated. From ${printValue(item.oldValue)} to ${printValue(item.newValue)}`);
-      }
-    }
+    const result = keys
+      .map((key) => {
+        const item = data[key];
+        if (item.type === 'unchanged' && item.valueType === 'complex') {
+          return printInner(item.value, [...prefix, key]);
+        }
+        if (item.type === 'added') {
+          return `Property '${printKey(key)}' was added with value: ${printValue(item.value)}`;
+        }
+        if (item.type === 'deleted') {
+          return `Property '${printKey(key)}' was removed`;
+        }
+        if (item.type === 'changed') {
+          return `Property '${printKey(key)}' was updated. From ${printValue(item.oldValue)} to ${printValue(item.newValue)}`;
+        }
+        return null;
+      })
+      .filter((el) => el !== null);
     return result.join('\n');
   };
   return printInner(value, []);
